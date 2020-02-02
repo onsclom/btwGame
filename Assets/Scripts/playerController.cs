6ff5ed5652e4c0f396ba54330b061c3bba21ede1
Austin Merrick <austinmerrick98@gmail.com>
2020-02-01 22:56:44 -0800

    First commit changes.

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    bool isGrounded = false;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    private Rigidbody2D rb;
+    private float dir=0;
+    public float speed;
+    private float extraSpeed=0f;
+    public float extraSpeedAmount;
+    public float jumpVel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, .15f, groundLayer);
+        dir = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                rb.AddForce(new Vector2 (0f, jumpVel));
+            }
+        }
+
+        //decrement extra speed
+        if (extraSpeed >= 0)
+        {
+            extraSpeed -= Time.deltaTime * 60;
+        }
+    }
+
+    private void FixedUpdate() 
+    {
+        rb.velocity = new Vector2 (dir * (speed+extraSpeed) * Time.deltaTime, rb.velocity.y);
+    }
+
+    public void speedup()
+    {
+        extraSpeed += extraSpeedAmount;
+    }
+}
---
+++ Assets/speedPowerup.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speedPowerup : MonoBehaviour
+{
+    public playerController player;
+    // Start is called before the first frame update
+    private Color activeColor;
+    public Color inactiveColor;
+    private bool active = true;
+    public float resetTime = 3f;
+    private float curTime = 0f;
+    void Start()
+    {
+        activeColor = GetComponent<SpriteRenderer>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (active)
+            GetComponent<SpriteRenderer>().color = activeColor;
+        else
+            GetComponent<SpriteRenderer>().color = inactiveColor;
+
+        if (!active)
+        {
+            curTime += Time.deltaTime;
+            if (curTime > resetTime)
+            {
+                curTime = 0;
+                active = true;
+            }
+        }
+    }
+
+    private void OnMouseEnter() 
+    {
+        if (active)
+        {
+            player.speedup();
+            active = false;
+        }
+    }
+}
